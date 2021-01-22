04d47264d215311f63bdad2bd3793c86d7d5e28d
beck_pham <chauvupham@gmail.com>
2021-01-22 14:58:41 -0800

    initial setup to transpile codeon web-browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,38 @@
 import * as esbuild from 'esbuild-wasm';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState(''); // the initial state of code that user write in the <textarea>
+  const [code, setCode] = useState(''); // the initial state of esbuild tool code in the <pre> element
+
 
   // startup web assembly
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm'
     });
-    console.log(service);
   };
   useEffect(() => {
     startService();
   }, []);
 
-  const [input, setInput] = useState(''); // the initial state of code that user write in the <textarea>
-  const [code, setCode] = useState(''); // the initial state of esbuild tool code in the <pre> element
 
-  const onClick = () => {
-    console.log(input)
-  }
+  const onClick = async() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx', // what kind of code
+      target: 'es2015'
+    });
+
+    setCode(result.code);
+  };
+
   return(
     <div>
       <textarea value ={input} onChange={e => setInput(e.target.value)}></textarea>
