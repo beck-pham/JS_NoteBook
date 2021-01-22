2365c0689e6005a26561975ad73cf0b327fd4b42
beck_pham <chauvupham@gmail.com>
2021-01-22 14:17:47 -0800

    initial set up web assembly helper for web-browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import * as esbuild from 'esbuild-wasm';
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  return <h1>Hi There</h1>;
+
+  // startup web assembly
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    console.log(service);
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const [input, setInput] = useState(''); // the initial state of code that user write in the <textarea>
+  const [code, setCode] = useState(''); // the initial state of esbuild tool code in the <pre> element
+
+  const onClick = () => {
+    console.log(input)
+  }
+  return(
+    <div>
+      <textarea value ={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
 }
 
 ReactDOM.render(
