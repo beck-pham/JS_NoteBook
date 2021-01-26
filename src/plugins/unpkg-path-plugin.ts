95545dd0674b023b172ab511319fd3dcabbf68b1
beck_pham <chauvupham@gmail.com>
2021-01-25 19:58:19 -0800

    implemented localForage for caching to improve web browser performance via indexedDB

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,18 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+}); 
+
+// (async () => {
+//   await fileCache.setItem('color', 'red');
+
+//   const color = fileCache.getItem('color');
+
+//   console.log(color);
+// })()
 
 export const unpkgPathPlugin = () => {
   return {
@@ -20,29 +33,38 @@ export const unpkgPathPlugin = () => {
 
         return {
           namespace: 'a',
-          path: `https://unpkg.com/${args.path}`
+          path: `https://unpkg.com/${args.path}`,
         }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
- 
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import React, { useState } from 'react';
+              import React, { useState } from 'react-select';
               console.log(React, useState);
             `,
           };
         }
+        // Check to see if we have already fetched this file
+        // and if it is in the cache || Typescript: using a generic function to describe the type of return value which is onLoadResult from interface onLoad > onLoadResult
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
         // parsing out property of built-in URL object
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult =  {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         }
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
