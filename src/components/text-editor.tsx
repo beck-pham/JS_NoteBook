5ab08851b21192d3f1b33af30594230696958c97
beck_pham <chauvupham@gmail.com>
2021-02-08 13:15:31 -0800

    styling and added state to text editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css';
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# HEADER');
+  //type annotation of HTML or Null with default starting value of Null
+  const ref = useRef<HTMLDivElement | null > (null);
 
+
+  // The solution here is going to be comparing whether or not that element is the element the user just clicked on is inside of that div.
+  // If it is inside, we're not going to flip the value of editing.
+  // Otherwise, if it is outside, we will update editing to be false.
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => { // annotation for event object
+      // check for ref pointing to the MDEditor <div>
+      // make sure event.target is defined
+      // compare the event target that clicked on is inside of that <div>
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) { // contains has a type definition of Node
+        return;
+      }
       setEditing(false);
     };
     document.addEventListener('click', listener, { capture: true });
@@ -15,20 +29,28 @@ const TextEditor: React.FC = () => {
     };
   }, []);
 
-
+  /** EDITOR COMPONENT */
   if (editing) {
     return(
-      <div>
-        <MDEditor />
+      <div className='text-editor' ref={ref}>
+        <MDEditor value={value} onChange={(value) => setValue(value || '')}/>
       </div>
     );
   }
-
+  /** PREVIEW COMPONENT */
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={ '# Header' } />
+    <div className='text-editor card' onClick={() => setEditing(true)}>
+      <div className='card-content'>
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   )
 }
 
 export default TextEditor;
+
+
+// So again, our solution here is going to be comparing whether or not that element just or whether or
+// not the element the user just clicked on is inside of that div and that's pretty much it.
+// If it is inside, we're not going to flip the value of editing.
+// Otherwise, if it is outside, we will update editing to be false.
